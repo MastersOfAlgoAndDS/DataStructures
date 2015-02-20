a36c844d267f7bb36c52ec478c260b95e6b87817
Milind Gokhale <milindhg@gmail.com>
2015-02-20 10:08:36 -0500

    Inserted code for default values of arrays

---
+++ src/DefaultValues.java
@@ -0,0 +1,14 @@
+
+public class DefaultValues {
+	public static void main(String[] args){
+		Boolean b1[] = new Boolean[10];
+		boolean b2[] = new boolean[10];
+		int[] d1 = new int[100];
+		Integer[] d2 = new Integer[100];
+		System.out.println("b1[1] = "+ b1[1]);
+		System.out.println("b2[1] = "+ b2[1]);
+		System.out.println("d1[1] = "+ d1[1]);
+		System.out.println("d2[1] = "+ d2[1]);
+		
+	}
+}
