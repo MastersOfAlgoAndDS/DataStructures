36b251399cc59182a5800d51e00d97ba63f2fda4
Milind Gokhale <milindhg@gmail.com>
2015-03-06 23:00:29 -0500

    infix to postfix and postfix expression evaluation programs from geeksforgeeks.com

--- src/geeksforgeeks/InfixToPostfix.java
+++ src/geeksforgeeks/InfixToPostfix.java
@@ -1,14 +1,69 @@
 package geeksforgeeks;
 
+import java.util.Stack;
+
 public class InfixToPostfix {
-	
-	public String InfixToPostfix(String infix){
+
+	public static String InfixToPostfix(String input) {
 		String postfix = "";
-		char[] infix
+		Stack<Character> st = new Stack<Character>();
+		st.push('$');
+		char[] infix = input.toCharArray();
+		for (int i = 0; i < infix.length; i++) {
+			char c = infix[i];
+			//char top = st.peek();
+			switch (c) {
+			case '(':
+				st.push(c);
+				break;
+			case ')':
+				while (st.peek() != '(') {
+					postfix = postfix.concat(st.pop().toString());
+				}
+				st.pop();
+				break;
+			case '-':
+			case '+':
+				while (st.peek() == '+' || st.peek() == '^' || st.peek() == '-'
+						|| st.peek() == '/' || st.peek() == '*') {
+					postfix = postfix.concat(st.pop().toString());
+				}
+				st.push(c);
+				break;
+			case '*':
+			case '/':
+				while (st.peek() == '*' || st.peek() == '^' || st.peek() == '/') {
+					postfix = postfix.concat(st.pop().toString());
+				}
+				st.push(c);
+				break;
+			case '^':
+				while (st.peek() == '^') {
+					postfix = postfix.concat(st.pop().toString());
+				}
+				st.push(c);
+				break;
+
+			default:
+				postfix = postfix.concat(Character.toString(c));
+			}
+		}
+		while (!st.isEmpty()) {
+			char temp = st.peek();
+			if(temp=='$')
+				break;
+			else
+				postfix = postfix.concat(st.pop().toString());
+		}
+
 		return postfix;
 	}
-	
-	public static void main(String[] args){
-		
+
+	public static void main(String[] args) {
+		//InfixToPostfix i2p = new InfixToPostfix();
+		System.out.println(InfixToPostfix("a+b*c").equals("abc*+") ? "Correct" : "WRONG!!!");
+		System.out.println(InfixToPostfix("(a+b)*c").equals("ab+c*") ? "Correct" : "WRONG!!!");
+		System.out.println(InfixToPostfix("a+b*(c^d-e)^(f+g*h)-i").equals("abcd^e-fgh*+^*+i-") ? "Correct" : "WRONG!!!");
+		System.out.println(InfixToPostfix("a+b*(c^d-e)^(f+g*h)-i"));
 	}
 }
---
+++ src/geeksforgeeks/PostfixEvaluation.java
@@ -0,0 +1,57 @@
+package geeksforgeeks;
+
+import java.util.Stack;
+
+public class PostfixEvaluation {
+
+	public static boolean isOperand(char character) {
+		switch (character) {
+		case '+':
+		case '-':
+		case '*':
+		case '/':
+		case '^':
+			return false;
+		default:
+			return true;
+		}
+	}
+
+	public static long postfixEvaluation(String input) {
+		long eval = 0;
+		char[] charArray = input.toCharArray();
+		Stack<Integer> st = new Stack<Integer>();
+		for (int i = 0; i < charArray.length; i++) {
+			char c = charArray[i];
+			if (isOperand(charArray[i])) {
+				st.push(Integer.valueOf(String.valueOf(charArray[i])));
+			} else {
+				int b = st.pop();
+				int a = st.pop();
+				switch (c) {
+				case '+':
+					st.push(a + b);
+					break;
+				case '-':
+					st.push(a - b);
+					break;
+				case '*':
+					st.push(a * b);
+					break;
+				case '/':
+					st.push(a / b);
+					break;
+				case '^':
+					st.push((int) Math.pow(a, b));
+					break;
+				}
+
+			}
+		}
+		return st.pop();
+	}
+	
+	public static void main(String[] args){
+		System.out.println(postfixEvaluation("231*+9-"));
+	}
+}
