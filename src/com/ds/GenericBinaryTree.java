864a40a970476a05457121463ff622a50462fcab
Milind Gokhale <milindhg@gmail.com>
2015-07-04 11:51:03 -0400

    Commented the GenericBinaryTree Program

--- src/com/ds/GenericBinaryTree.java
+++ src/com/ds/GenericBinaryTree.java
@@ -1,18 +1,35 @@
-package com.ds;
+/*
+ * This program is specially written for easier and quicker test data preparation
+ * of various problems related to binary trees.
+ * The input needs to be a ArrayList of the data structure whose tree is to be created.
+ * 
+ * */
 
+package com.ds;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Scanner;
 
+/**
+ * @author Milind This program is specially written for easier and quicker test
+ *         data preparation of various problems related to binary trees. The
+ *         input needs to be a ArrayList of the data structure whose tree is to
+ *         be created.
+ * 
+ * @param <T>
+ */
 public class GenericBinaryTree<T> {
-	GenericBinaryTreeNode<T> root; 
+	GenericBinaryTreeNode<T> root;
 
 	public GenericBinaryTree() {
 		root = null;
 	}
 
-	/**contructor
+	/**
+	 * contructor The contructor is responsible for creation of the tree so that
+	 * the tree is returned on the new contructor call.
+	 * 
 	 * @param list
 	 */
 	public GenericBinaryTree(ArrayList<T> list) {
@@ -20,68 +37,73 @@ public class GenericBinaryTree<T> {
 		int iterator = 0;
 		boolean currTreeNodeFullFlag = false;
 		LinkedList<GenericBinaryTreeNode<T>> queue = new LinkedList<GenericBinaryTreeNode<T>>();
-		if(list.isEmpty())
-			root=null;
-		else{
+		if (list.isEmpty())
+			root = null;
+		else {
 			root = new GenericBinaryTreeNode<T>(list.get(iterator));
-//			root.setVal(list.get(iterator));
-//			root.setLeft(null);
-//			root.setRight(null);
 			iterator++;
 			queue.add(root);
 		}
-		while(iterator<len && !queue.isEmpty()){
+		while (iterator < len && !queue.isEmpty()) {
 			T nextListItem = list.get(iterator);
-			GenericBinaryTreeNode<T> nextTreeNode = new GenericBinaryTreeNode<T>(nextListItem);
+			GenericBinaryTreeNode<T> nextTreeNode = new GenericBinaryTreeNode<T>(
+					nextListItem);
 			GenericBinaryTreeNode<T> currQueueItem = queue.peek();
-			if(!isTreeNodeRipe(currQueueItem))
-			{
-				if(currQueueItem.getLeft()==null)
+			if (!isTreeNodeRipe(currQueueItem)) {
+				if (currQueueItem.getLeft() == null)
 					currQueueItem.setLeft(nextTreeNode);
-				else if (currQueueItem.getRight()==null)
-				{
+				else if (currQueueItem.getRight() == null) {
 					currQueueItem.setRight(nextTreeNode);
 					queue.remove();
 				}
 			}
-//			currQueueItem.setLeft(new GenericBinaryTreeNode<T>(list.get(iterator)));
+			// currQueueItem.setLeft(new
+			// GenericBinaryTreeNode<T>(list.get(iterator)));
 			queue.add(nextTreeNode);
 			iterator++;
-//			if(iterator<len){
-//				currQueueItem.setRight(new GenericBinaryTreeNode<T>(list.get(iterator)));
-//				queue.add(currQueueItem.getRight());
-//				iterator++;
-//			}
+			// if(iterator<len){
+			// currQueueItem.setRight(new
+			// GenericBinaryTreeNode<T>(list.get(iterator)));
+			// queue.add(currQueueItem.getRight());
+			// iterator++;
+			// }
 		}
 	}
-	
-	
+
 	/**
-	 * Returns true if the tree node is ripe 
-	 * i.e. both left and right children of the tree node are assigned.
-	 * i.e. both left and right are not null 
+	 * Returns true if the tree node is ripe i.e. both left and right children
+	 * of the tree node are assigned. i.e. both left and right are not null
+	 * 
 	 * @param currQueueItem
 	 * @return
 	 */
 	private boolean isTreeNodeRipe(GenericBinaryTreeNode<T> currQueueItem) {
-		if(currQueueItem.getLeft()!=null && currQueueItem.getRight()!=null)
+		if (currQueueItem.getLeft() != null && currQueueItem.getRight() != null)
 			return true;
 		return false;
 	}
-	
-	
 
+	/*
+	 * This toString function prints the tree with parentheses (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
 	@Override
 	public String toString() {
-//		return "GenericBinaryTree [root=" + root + "]";
+		// return "GenericBinaryTree [root=" + root + "]";
 		return "[" + root + "]";
 	}
 
+	/**
+	 * Test function for testing various functions of the tree program
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int numberOfNodes = in.nextInt();
 		ArrayList<Integer> list = new ArrayList<Integer>();
-		for(int i=0; i<numberOfNodes; i++){
+		for (int i = 0; i < numberOfNodes; i++) {
 			list.add(in.nextInt());
 		}
 		GenericBinaryTree<Integer> tree = new GenericBinaryTree<Integer>(list);
