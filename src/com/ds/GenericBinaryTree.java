61e7919fd1d52e2f10a80b2bd5bd937192eab0fd
Milind Gokhale <milindhg@gmail.com>
2015-07-11 18:14:38 -0400

    Added code for improved tree diameter and refactored the main test client with multiple functions for easy testing.

--- src/com/ds/GenericBinaryTree.java
+++ src/com/ds/GenericBinaryTree.java
@@ -1,9 +1,11 @@
-/*
- * This program is specially written for easier and quicker test data preparation
- * of various problems related to binary trees.
- * The input needs to be a ArrayList of the data structure whose tree is to be created.
+/**
+ * @author Milind Gokhale
  * 
- * Includes
+ * @Info This program is specially written for easier and quicker test data
+ *      preparation of various problems related to binary trees. The input needs
+ *      to be a ArrayList of the data structure whose tree is to be created.
+ * 
+ * 	Includes
  * 	Basic Binary tree construction
  *  Add node
  *  Inorder tree traversal
@@ -32,10 +34,12 @@ import java.util.Scanner;
 import java.util.Stack;
 
 /**
- * @author Milind This program is specially written for easier and quicker test
- *         data preparation of various problems related to binary trees. The
- *         input needs to be a ArrayList of the data structure whose tree is to
- *         be created.
+ * @author Milind Gokhale
+ * 
+ * @Info This program is specially written for easier and quicker test data
+ *       preparation of various problems related to binary trees. The input
+ *       needs to be a ArrayList of the data structure whose tree is to be
+ *       created.
  * 
  * @param <T>
  */
@@ -106,7 +110,7 @@ public class GenericBinaryTree<T> {
 	 * of the tree node are assigned. i.e. both left and right are not null
 	 * 
 	 * @param currQueueItem
-	 * @return
+	 * @return boolean
 	 */
 	private boolean isTreeNodeRipe(GenericBinaryTreeNode<T> currQueueItem) {
 		if (currQueueItem.getLeft() != null && currQueueItem.getRight() != null)
@@ -129,7 +133,7 @@ public class GenericBinaryTree<T> {
 	 * Function to print the preorder traversal of the tree output
 	 * 
 	 * @param head
-	 * @return
+	 * @return void
 	 */
 	public void preOrder(GenericBinaryTreeNode<T> head) {
 		if (head == null)
@@ -145,7 +149,7 @@ public class GenericBinaryTree<T> {
 	 * Function to print the inorder traversal of the tree output
 	 * 
 	 * @param head
-	 * @return
+	 * @return void
 	 */
 	public void inOrder(GenericBinaryTreeNode<T> head) {
 		if (head != null) {
@@ -159,7 +163,7 @@ public class GenericBinaryTree<T> {
 	 * Function to print the postorder traversal of the tree output
 	 * 
 	 * @param head
-	 * @return
+	 * @return void
 	 */
 	public void postOrder(GenericBinaryTreeNode<T> head) {
 		if (head != null) {
@@ -174,6 +178,7 @@ public class GenericBinaryTree<T> {
 	 * Function to print the tree in level order (level-by-level)
 	 * 
 	 * @param head
+	 * @return void
 	 */
 	public void levelOrder(GenericBinaryTreeNode<T> head) {
 		if (head != null) {
@@ -196,7 +201,7 @@ public class GenericBinaryTree<T> {
 	 * Function to calculate the size of the tree
 	 * 
 	 * @param head
-	 * @return
+	 * @return int
 	 */
 	public int size(GenericBinaryTreeNode<T> head) {
 		if (head == null)
@@ -212,7 +217,7 @@ public class GenericBinaryTree<T> {
 	 * 
 	 * @param head1
 	 * @param head2
-	 * @return
+	 * @return boolean
 	 */
 	public boolean sameTrees(GenericBinaryTreeNode<T> head1,
 			GenericBinaryTreeNode<T> head2) {
@@ -231,7 +236,8 @@ public class GenericBinaryTree<T> {
 	 * Function to calculate the height of the tree. I.e. maximum depth if the
 	 * tree is unbalanced.
 	 * 
-	 * @return
+	 * @param head
+	 * @return int
 	 */
 	public int height(GenericBinaryTreeNode<T> head) {
 		if (head == null)
@@ -239,10 +245,23 @@ public class GenericBinaryTree<T> {
 		return Math.max(height(head.getLeft()), height(head.getRight())) + 1;
 	}
 
+	/**
+	 * Function to delete a tree
+	 * 
+	 * @param head
+	 * @return void
+	 */
 	public void deleteTree(GenericBinaryTreeNode<T> head) {
 		root = null;
 	}
 
+	/**
+	 * Function to convert a tree into its mirror. i.e. left becomes right and
+	 * right becomes left
+	 * 
+	 * @param head
+	 * @return mirrored tree head
+	 */
 	public GenericBinaryTreeNode<T> mirrorTree(GenericBinaryTreeNode<T> head) {
 		if (head == null)
 			return null;
@@ -258,6 +277,14 @@ public class GenericBinaryTree<T> {
 		}
 	}
 
+	/**
+	 * Function to print the tree paths from the root to each of the leaves.
+	 * 
+	 * @param head
+	 * @param path
+	 * @param pathlen
+	 * @return void
+	 */
 	public void printPaths(GenericBinaryTreeNode<T> head, T[] path, int pathlen) {
 		if (head == null)
 			return;
@@ -278,6 +305,12 @@ public class GenericBinaryTree<T> {
 		}
 	}
 
+	/**
+	 * Function to count the number of leaf nodes in the tree.
+	 * 
+	 * @param head
+	 * @return int
+	 */
 	public int getLeafCount(GenericBinaryTreeNode<T> head) {
 		if (head == null)
 			return 0;
@@ -292,6 +325,7 @@ public class GenericBinaryTree<T> {
 	 * and R>L at alternate levels
 	 * 
 	 * @param head
+	 * @return void
 	 */
 	public void levelOrderSpiral(GenericBinaryTreeNode<T> head) {
 		Stack<GenericBinaryTreeNode<T>> s1 = new Stack<GenericBinaryTreeNode<T>>();
@@ -328,7 +362,7 @@ public class GenericBinaryTree<T> {
 	 * false.
 	 * 
 	 * @param head
-	 * @return
+	 * @return boolean
 	 */
 	public boolean isChildrenSumPropertyValid(
 			GenericBinaryTreeNode<Integer> head) {
@@ -342,110 +376,306 @@ public class GenericBinaryTree<T> {
 		else
 			return false;
 	}
-	
-	
+
 	/**
-	 * Function to calculate the diameter of a tree.
-	 * Diameter is the longest path from any of the leaf to another leaf in the tree.
-	 * This diameter may or may not pass via the root.
-	 * Basically diameter is the max of the following 3
-	 * 		max height on left subtree + max height on right subtree + 1
-	 * 		longest diameter in the left subtree
-	 * 		longest diameter in the right subtree
+	 * Function to calculate the diameter of a tree. Diameter is the longest
+	 * path from any of the leaf to another leaf in the tree. This diameter may
+	 * or may not pass via the root. Basically diameter is the max of the
+	 * following 3 max height on left subtree + max height on right subtree + 1
+	 * longest diameter in the left subtree longest diameter in the right
+	 * subtree
+	 * 
 	 * @param head
+	 * @return int
 	 */
-	public int diameter(GenericBinaryTreeNode<T> head){
-		if (head==null)
+	public int diameter(GenericBinaryTreeNode<T> head) {
+		if (head == null)
 			return 0;
-		int leftHt=0, rightHt=0;
+		int leftHt = 0, rightHt = 0;
 		leftHt = height(head.getLeft());
 		rightHt = height(head.getRight());
-		return Math.max(leftHt + rightHt + 1,Math.max(diameter(head.getLeft()), diameter(head.getRight())));
+		return Math.max(leftHt + rightHt + 1,
+				Math.max(diameter(head.getLeft()), diameter(head.getRight())));
+	}
+
+	public int diameterImproved(GenericBinaryTreeNode<T> head,
+			GenericNode<Integer> height) {
+		if (head == null) {
+			height.setVal(0);
+			return 0;
+		}
+		GenericNode<Integer> leftHt = new GenericNode<Integer>(0);
+		GenericNode<Integer> rightHt = new GenericNode<Integer>(0);
+
+		int lDiam = 0, rDiam = 0;
+		lDiam = diameterImproved(head.getLeft(), leftHt);
+		rDiam = diameterImproved(head.getRight(), rightHt);
+
+		height.setVal(Math.max(leftHt.getVal(), rightHt.getVal()) + 1);
+		return Math.max(leftHt.getVal() + rightHt.getVal() + 1,
+				Math.max(lDiam, rDiam));
 	}
 
 	/**
-	 * Test function for testing various functions of the tree program
+	 * Main Test Client for testing various functions of the tree program
 	 * 
 	 * @param args
+	 * @return void
 	 */
 	public static void main(String[] args) {
-//		Scanner in = new Scanner(System.in);
-//		int numberOfNodes = in.nextInt();
-//		ArrayList<Integer> list = new ArrayList<Integer>();
-//		for (int i = 0; i < numberOfNodes; i++) {
-//			list.add(in.nextInt());
-//		}
-//		GenericBinaryTree<Integer> tree = new GenericBinaryTree<Integer>(list);
-//		System.out.println(tree);
-//		tree.preOrder(tree.getRoot());
-//		System.out.println();
-//		tree.inOrder(tree.getRoot());
-//		System.out.println();
-//		tree.postOrder(tree.getRoot());
-//		System.out.println();
-//		System.out.println("Tree size is: " + tree.size(tree.getRoot()));
-//
-//		Scanner in2 = new Scanner(System.in);
-//		int numberOfNodes2 = in2.nextInt();
-//		ArrayList<Integer> list2 = new ArrayList<Integer>();
-//		for (int i = 0; i < numberOfNodes2; i++) {
-//			list2.add(in2.nextInt());
-//		}
-//		GenericBinaryTree<Integer> tree2 = new GenericBinaryTree<Integer>(list2);
-//		System.out.println(tree.sameTrees(tree.getRoot(), tree2.getRoot()));
-//
-//		System.out.println("Height of the tree is: "
-//				+ tree.height(tree.getRoot()));
-//		GenericBinaryTree<Integer> treeRandom = new GenericBinaryTree<Integer>();
-//		treeRandom.setRoot(new GenericBinaryTreeNode<Integer>(20));
-//		treeRandom.getRoot().setLeft(new GenericBinaryTreeNode<Integer>(10));
-//		treeRandom.getRoot().getLeft()
-//				.setLeft(new GenericBinaryTreeNode<Integer>(30));
-//		treeRandom.getRoot().getLeft().getLeft()
-//				.setLeft(new GenericBinaryTreeNode<Integer>(40));
-//		treeRandom.getRoot().setRight(new GenericBinaryTreeNode<Integer>(70));
-//		System.out.println(treeRandom.toString());
-//		System.out.println("Height of random tree is: "
-//				+ treeRandom.height(treeRandom.getRoot()));
-//
-//		System.out.println("Tree before deleting: " + treeRandom);
-//		treeRandom.deleteTree(treeRandom.getRoot());
-//		System.out.println("Tree after deleting: " + treeRandom);
-//
-//		System.out.println("Before mirroring the tree: ");
-//		System.out.println(tree.getRoot());
-//		System.out.println("After mirroring the tree: ");
-//		System.out.println(tree.mirrorTree(tree.getRoot()));
-//		tree.mirrorTree(tree.getRoot());
-//		// Object[] a = new Object[1000];
-//		// tree.printPaths(tree.getRoot(),(Integer[]) a,0);
-//		System.out.println();
-//		tree.levelOrder(tree.getRoot());
-//		System.out.println("Number of leaf nodes is: "
-//				+ tree.getLeafCount(tree.getRoot()));
-//
-//		tree.levelOrderSpiral(tree.getRoot());
-//
-//		ArrayList<Integer> list3 = new ArrayList<Integer>();
-//		list3.add(10);
-//		list3.add(8);
-//		list3.add(2);
-//		list3.add(3);
-//		list3.add(5);
-//		list3.add(2);
-//
-//		GenericBinaryTree<Integer> tree3 = new GenericBinaryTree<Integer>(list3);
-//		System.out.println(tree3.isChildrenSumPropertyValid(tree3.getRoot()));
-
-		//diameter test
-		ArrayList<Integer> list4 = new ArrayList<Integer>();
-		list4.add(1);
-		list4.add(2);
-		list4.add(3);
-		list4.add(4);
-		list4.add(5);
-		GenericBinaryTree<Integer> tree4 = new GenericBinaryTree<Integer>(list4);
-		System.out.println("Diameter of tree4 is:" + tree4.diameter(tree4.getRoot()));
+		// Object[] a = new Object[1000];
+		// tree.printPaths(tree.getRoot(),(Integer[]) a,0);
+		GenericBinaryTree<Integer> tree = createSampleTree(999999);
+		long start=System.currentTimeMillis();
+		testDiameterImproved(tree);
+		long time = System.currentTimeMillis()-start;
+		System.out.println("Time required for this function: " + time + " secs");
+		
+		start=System.currentTimeMillis();
+		testDiameter(tree);
+		time = System.currentTimeMillis()-start;
+		System.out.println("Time required for this function: " + time + " secs");
 		
 	}
+
+	/**
+	 * Function to take input of the tree from user
+	 * 
+	 * @return tree
+	 */
+	public static GenericBinaryTree takeTreeInput() {
+		Scanner in = new Scanner(System.in);
+		int numberOfNodes = in.nextInt();
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		for (int i = 0; i < numberOfNodes; i++) {
+			list.add(in.nextInt());
+		}
+		GenericBinaryTree<Integer> tree = new GenericBinaryTree<Integer>(list);
+		System.out.println("Tree Created.");
+		System.out.println(tree);
+		return tree;
+	}
+
+	/**
+	 * Function to print the various tree traversals taking the input of the
+	 * treeas paramter.
+	 * 
+	 * @param tree
+	 * @return void
+	 */
+	public static void testTreeTraversals(GenericBinaryTree tree) {
+		tree.preOrder(tree.getRoot());
+		System.out.println();
+		tree.inOrder(tree.getRoot());
+		System.out.println();
+		tree.postOrder(tree.getRoot());
+		System.out.println();
+		tree.levelOrder(tree.getRoot());
+		System.out.println();
+		tree.levelOrderSpiral(tree.getRoot());
+
+	}
+
+	/**
+	 * Function to test the mirror tree function
+	 * 
+	 * @return void
+	 */
+	public static void testMirrorTree() {
+		GenericBinaryTree tree = createSampleTree();
+		System.out.println("Before mirroring the tree: ");
+		System.out.println(tree.getRoot());
+		System.out.println("After mirroring the tree: ");
+		System.out.println(tree.mirrorTree(tree.getRoot()));
+		tree.mirrorTree(tree.getRoot());
+
+	}
+
+	/**
+	 * Function to test the delete tree function
+	 * 
+	 * @return void
+	 */
+	public static void testDeleteTree() {
+		GenericBinaryTree<Integer> treeRandom = new GenericBinaryTree<Integer>();
+		treeRandom.setRoot(new GenericBinaryTreeNode<Integer>(20));
+		treeRandom.getRoot().setLeft(new GenericBinaryTreeNode<Integer>(10));
+		treeRandom.getRoot().getLeft()
+				.setLeft(new GenericBinaryTreeNode<Integer>(30));
+		treeRandom.getRoot().getLeft().getLeft()
+				.setLeft(new GenericBinaryTreeNode<Integer>(40));
+		treeRandom.getRoot().setRight(new GenericBinaryTreeNode<Integer>(70));
+		System.out.println(treeRandom.toString());
+		System.out.println("Height of random tree is: "
+				+ treeRandom.height(treeRandom.getRoot()));
+
+		System.out.println("Tree before deleting: " + treeRandom);
+		treeRandom.deleteTree(treeRandom.getRoot());
+		System.out.println("Tree after deleting: " + treeRandom);
+
+	}
+
+	/**
+	 * Function to test the sametrees function.
+	 * 
+	 * @return void
+	 * 
+	 */
+	public static void testIdenticalTrees() {
+		GenericBinaryTree tree1 = createSampleTree();
+		GenericBinaryTree tree2 = createSampleTree();
+		tree1.sameTrees(tree1.getRoot(), tree2.getRoot());
+	}
+
+	/**
+	 * Function to create a sample tree for testing. This is the function mostly
+	 * changed for any test performed.
+	 * 
+	 * @return tree
+	 */
+	public static GenericBinaryTree createSampleTree() {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(5);
+		GenericBinaryTree<Integer> tree = new GenericBinaryTree<Integer>(list);
+		return tree;
+	}
+
+	/**
+	 * Function to create a sample tree for testing given input number of nodes
+	 * in the tree. This is the function mostly changed for any test performed.
+	 * @param int numOfNodes
+	 * @return tree
+	 */
+	public static GenericBinaryTree createSampleTree(int numOfNodes) {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		for(int i=1; i<=numOfNodes; i++){
+			list.add(i);
+		}
+		GenericBinaryTree<Integer> tree = new GenericBinaryTree<Integer>(list);
+		return tree;
+	}
+
+	/**
+	 * Function to test the height and size of the tree functions.
+	 * 
+	 * @return void
+	 */
+	public static void testHeightSizeAndLeafCount() {
+		GenericBinaryTree tree = createSampleTree();
+		System.out.println();
+		System.out.println("Tree size is: " + tree.size(tree.getRoot()));
+		System.out.println("Height of the tree is: "
+				+ tree.height(tree.getRoot()));
+		System.out.println("Number of leaf nodes is: "
+				+ tree.getLeafCount(tree.getRoot()));
+
+	}
+
+	/**
+	 * Function to test isChildrenSumPropertyValid tree function
+	 * 
+	 * @return void
+	 */
+	public static void testIsChildrenSumProperty() {
+		ArrayList<Integer> list3 = new ArrayList<Integer>();
+		list3.add(10);
+		list3.add(8);
+		list3.add(2);
+		list3.add(3);
+		list3.add(5);
+		list3.add(2);
+
+		GenericBinaryTree<Integer> tree3 = new GenericBinaryTree<Integer>(list3);
+		System.out.println(tree3.isChildrenSumPropertyValid(tree3.getRoot()));
+
+	}
+
+	/**
+	 * Function to test the diameter function.
+	 * 
+	 * @return void
+	 */
+	public static void testDiameter() {
+		// diameter test
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		// list4.add(1);
+		// list4.add(2);
+		// list4.add(3);
+		// list4.add(4);
+		// list4.add(5);
+
+		list.add(10);
+		list.add(15);
+		list.add(5);
+		list.add(7);
+		list.add(4);
+		list.add(3);
+		list.add(12);
+		list.add(6);
+		list.add(17);
+
+		GenericBinaryTree<Integer> tree = new GenericBinaryTree<Integer>(list);
+		System.out.println("Diameter of tree is:"
+				+ tree.diameter(tree.getRoot()));
+	}
+
+	/**
+	 * Function to test the diameter function given a tree as input.
+	 * 
+	 * @param GenericBinaryTree
+	 *            tree
+	 * @return void
+	 */
+	public static void testDiameter(GenericBinaryTree tree) {
+		System.out.println("Diameter of tree is:"
+				+ tree.diameter(tree.getRoot()));
+	}
+
+	/**
+	 * Function to test the diameter function.
+	 * 
+	 * @return void
+	 */
+	public static void testDiameterImproved() {
+		// diameter test
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		// list4.add(1);
+		// list4.add(2);
+		// list4.add(3);
+		// list4.add(4);
+		// list4.add(5);
+
+		list.add(10);
+		list.add(15);
+		list.add(5);
+		list.add(7);
+		list.add(4);
+		list.add(3);
+		list.add(12);
+		list.add(6);
+		list.add(17);
+
+		GenericBinaryTree<Integer> tree = new GenericBinaryTree<Integer>(list);
+		System.out.println("Diameter of tree with improved algorithm is:"
+				+ tree.diameterImproved(tree.getRoot(),
+						new GenericNode<Integer>(0)));
+	}
+
+	/**
+	 * Function to test the diameter function given a tree as input.
+	 * 
+	 * @param GenericBinaryTree
+	 *            tree
+	 * @return void
+	 */
+	public static void testDiameterImproved(GenericBinaryTree tree) {
+		System.out.println("Diameter of tree with improved algorithm is:"
+				+ tree.diameterImproved(tree.getRoot(),
+						new GenericNode<Integer>(0)));
+	}
+
 }
