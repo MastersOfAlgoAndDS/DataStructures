e88e5405b4a584363230330c5d12fd8b40a0fe58
Milind Gokhale <milindhg@gmail.com>
2015-07-11 18:16:34 -0400

    Added the generic node for solving the pointer based problems using the linked list node instead of the pointer as used in the C programming language.

---
+++ src/com/ds/GenericNode.java
@@ -0,0 +1,39 @@
+/**
+ * Basic Tree Node. This node is generic i.e. the content can be anything. 
+ * The datatype of the content of the node can be anything. 
+ * So we can can make a tree of any type of object.
+ * 
+ * */
+
+package com.ds;
+
+public class GenericNode<T> {
+
+	T val;
+	GenericNode<T> next;
+	
+	public GenericNode(){
+		val = null;
+		next = null;
+	}
+	
+	public GenericNode(T t){
+		val = t;
+	}
+	
+	public T getVal() {
+		return val;
+	}
+	public void setVal(T val) {
+		this.val = val;
+	}
+	public GenericNode<T> getNext() {
+		return next;
+	}
+	public void setNext(GenericNode<T> next) {
+		this.next = next;
+	}
+	
+	
+	
+}
