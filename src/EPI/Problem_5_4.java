c33f639742a79def5697c33bc5903275be55de82
Milind Gokhale <milindhg@gmail.com>
2015-08-03 20:31:13 -0400

    GCD program started

---
+++ src/EPI/Problem_5_4.java
@@ -0,0 +1,38 @@
+package EPI;
+
+public class Problem_5_4 {
+
+	/**
+	 * 
+	 * Simple logic: Keep moding the numbers as (large mod small) until the mod answer is zero. 
+	 * when mod is zero, smaller number is the answer.
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	public int simpleGCD(int x, int y) {
+		int z = x;
+		if (x < y) {
+			//since x is smaller than y, swap x and y.
+			x = x ^ y;
+			y = x ^ y;
+			x = x ^ y;
+			z = x;
+		}
+		if(y == 1){
+			return z;
+		}
+		while (x % y != 0) {
+			z = x % y;
+			x = y;
+			y = z;
+		}
+		return z;
+	}
+
+	public static void main(String[] args) {
+		Problem_5_4 p = new Problem_5_4();
+		System.out.println(p.simpleGCD(1, 32));
+	}
+
+}
