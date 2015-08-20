9ac5eca16b5533e7eee48560297a6d45d41791b6
Milind Gokhale <milindhg@gmail.com>
2015-08-20 08:48:38 -0400

    fixed toString for node, which helps in properly debugging and seeing the content in node.

--- src/com/ds/Node.java
+++ src/com/ds/Node.java
@@ -3,24 +3,42 @@ package com.ds;
 public class Node {
 	private int val;
 	private Node next;
-	
-	public Node(int value){
+
+	public Node(int value) {
 		val = value;
 		next = null;
 	}
-	
+
 	public int getVal() {
 		return val;
 	}
+
 	public void setVal(int value) {
 		this.val = value;
 	}
+
 	public Node getNext() {
 		return next;
 	}
+
 	public void setNext(Node next) {
 		this.next = next;
 	}
-	
-	
+
+	@Override
+	public String toString() {
+		return "Node [val=" + val + ", next="
+				+ (next == null ? null : next.getVal()) + "]";
+	}
+
+	public void printNode() {
+		Node trav = this;
+		System.out.print("head");
+		while (trav != null) {
+			System.out.print("->" + trav.getVal());
+			trav = trav.getNext();
+		}
+		System.out.println();
+	}
+
 }
