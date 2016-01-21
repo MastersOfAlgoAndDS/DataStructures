d51cbba3a1f2893d798dc8d3d8fced0780f1a16f
milindhg <milindhg@gmail.com>
2016-01-21 13:50:43 -0500

    synced yaiznayut incomplete files

---
+++ src/EPI/Problem_5_5.java
@@ -0,0 +1,15 @@
+/**
+ * Problem 5.5 : Given two positive integers x and y, how would you compute x=y if
+ * the only operators you can use are addition, subtraction, and shifting?
+ * */
+
+package EPI;
+
+public class Problem_5_5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/LiveRamp/Solution.java
@@ -0,0 +1,61 @@
+package LiveRamp;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class Solution {
+	public int[] solution(int[] T) {
+		int n = T.length;
+		int capital = -1;
+		for (int i = 0; i < n; i++) {
+			if (T[i] == i)
+				capital = i;
+		}
+		HashMap<Integer, LinkedList<Integer>> hash = new HashMap<Integer, LinkedList<Integer>>();
+		for (int i = 0; i < n; i++) {
+			if (!hash.containsKey(T[i])) {
+				LinkedList<Integer> ll = new LinkedList<Integer>();
+				ll.add(i);
+				hash.put(T[i], ll);
+			} else if (hash.containsKey(T[i])) {
+				hash.get(T[i]).addLast(i);
+			}
+		}
+
+		// get the cities and count
+		int[] result = new int[T.length];
+		int cnt = 1;
+		int cityIter = capital;
+		System.out.println(capital);
+		for (Integer i : hash.get(cityIter)) {
+			int cityCnt = 0;
+			for (Integer j : hash.get(i)) {
+				System.out.println(j);
+				if (j == cityIter)
+					continue;
+				result[cnt] += 1;
+
+				if (hash.containsKey(j)) {
+					cityCnt += hash.get(j).size();
+					cityIter = j;
+				}
+			}
+
+			cnt++;
+			// result[cnt] = hash.get(capital).size() - 1;
+
+		}
+		for (int i = 0; i < n - 1; i++)
+			result[i] = result[i + 1];
+		return result;
+	}
+
+	public static void main(String[] args) {
+		int[] T = { 9, 1, 4, 9, 0, 4, 8, 9, 0, 1 };
+		Solution s = new Solution();
+		int[] result = s.solution(T);
+		for (int i : result)
+			System.out.print(i + " ");
+	}
+
+}
