94c6a8a2bb16eda01ab275bda9d41b9e451f7db6
milindhg <milindhg@gmail.com>
2016-01-29 20:20:29 -0500

    problem 1_7 done - zero matrix

---
+++ src/crackingthecodinginterview/P1_7_Zero_Matrix.java
@@ -0,0 +1,78 @@
+package crackingthecodinginterview;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @Problem_1_7 Write an algorithm such that if an element in an MxN matrix is
+ *              0, its entire row and column are set to 0.
+ * 
+ * @author Milind
+ *
+ */
+public class P1_7_Zero_Matrix {
+
+	private int[][] matrix;
+
+	public int[][] getMatrix() {
+		return matrix;
+	}
+
+	public void setMatrix(int[][] matrix) {
+		this.matrix = matrix;
+	}
+
+	public void print() {
+		int[][] arr = getMatrix();
+		int n = arr[0].length;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		P1_7_Zero_Matrix s = new P1_7_Zero_Matrix();
+		int[][] arr = { { 1, 1, 1 }, { 1, 1, 1 }, { 0, 1, 0 } };
+		s.setMatrix(arr);
+		s.print();
+		System.out.println();
+		s.makeZero();
+		s.print();
+	}
+
+	private void makeZero() {
+		// capture all zero containing rows and cols
+		int[][] arr = getMatrix();
+		int colCnt = arr[0].length;
+		int rowCnt = arr.length;
+
+		Set<Integer> zeroRows = new HashSet<Integer>();
+		Set<Integer> zeroCols = new HashSet<Integer>();
+		for (int i = 0; i < rowCnt; i++) {
+			for (int j = 0; j < colCnt; j++) {
+				if (arr[i][j] == 0) {
+					zeroRows.add(i);
+					zeroCols.add(j);
+				}
+			}
+		}
+
+		// now run loop on zeroRows to set all elements of those rows to zero
+		for (Integer i : zeroRows) {
+			for (int j = 0; j < colCnt; j++) {
+				arr[i][j] = 0;
+			}
+		}
+
+		// now run loop on zeroCols to set all elements of those cols to zero
+		for (Integer j : zeroCols) {
+			for (int i = 0; i < rowCnt; i++) {
+				arr[i][j] = 0;
+			}
+		}
+		setMatrix(arr);
+	}
+}
