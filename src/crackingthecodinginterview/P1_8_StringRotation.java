6a2f81a1f3fa4c078ab380170e46b6135ace4f30
milindhg <milindhg@gmail.com>
2016-01-29 21:38:35 -0500

    problem 1_8 done Check if a string is a rotation of given original string

---
+++ src/crackingthecodinginterview/P1_8_StringRotation.java
@@ -0,0 +1,40 @@
+package crackingthecodinginterview;
+
+/**
+ * @Problem_1_8 Assume you have a method isSubstring which checks if one word is
+ *              a substring of another. Given two strings, si and s2, write code
+ *              to check if s2 is a rotation of si using only one call to
+ *              isSubstring (e.g.,"waterbottle"is a rotation of "erbottlewat").
+ * 
+ * @author Milind
+ *
+ */
+public class P1_8_StringRotation {
+
+	public static void main(String[] args) {
+		P1_8_StringRotation s = new P1_8_StringRotation();
+		String s1 = "waterbottle";
+		String s2 = "erbottlewat";
+		System.out.println(s.isRotation(s1, s2));
+
+	}
+
+	/**
+	 * String the string s1 twice like "waterbottlewaterbottle" and search
+	 * "erbottlewat" as a substring in s1. Also check if the length of both s1
+	 * and s2 are same. Because the logic will also give true for input
+	 * "erbottlewa" where last t is not given. So the length does not match.
+	 * 
+	 * @param s1
+	 * @param s2
+	 * @return
+	 */
+	private boolean isRotation(String s1, String s2) {
+		return isSubstring(s1 + s1, s2) && s1.length() == s2.length();
+	}
+
+	private boolean isSubstring(String s1, String s2) {
+		return s1.contains(s2);
+	}
+
+}
