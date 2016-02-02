ab964b337edcbf0a3be4fa3decd42d727e3f18d5
milindhg <milindhg@gmail.com>
2016-02-01 19:39:40 -0500

    Problem 2_2 done

---
+++ src/ctci_5E/P_2_2_KthToLastElement.java
@@ -0,0 +1,46 @@
+package ctci_5E;
+
+import com.ds.Node;
+import com.ds.SinglyLinkedList;
+
+/**
+ * 
+ * @Problem_2_2 Implement an algorithm to find the kth to last element of a
+ *              singly linked list.
+ * 
+ * @author Milind
+ *
+ */
+public class P_2_2_KthToLastElement {
+
+	public static void main(String[] args) {
+		P_2_2_KthToLastElement s = new P_2_2_KthToLastElement();
+		SinglyLinkedList ll = new SinglyLinkedList();
+		ll.insertAtLast(3);
+		ll.insertAtLast(5);
+		ll.insertAtLast(4);
+		ll.insertAtLast(3);
+		ll.print(ll.getHead());
+		Node elem = s.kthToLastElement(ll, 10);
+		if (elem != null) {
+			System.out.println(elem.getVal());
+		}
+	}
+
+	private Node kthToLastElement(SinglyLinkedList ll, int k) {
+		if (ll != null && ll.getHead() != null) {
+			Node temp = ll.getHead();
+			Node prev = temp;
+			while (k > 0 && temp != null) {
+				temp = temp.getNext();
+				k--;
+			}
+			while (temp != null) {
+				temp = temp.getNext();
+				prev = prev.getNext();
+			}
+			return prev;
+		}
+		return null;
+	}
+}
