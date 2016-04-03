2a2953ada8229785421a240f7c6c1a546d082854
milindhg <milindhg@gmail.com>
2016-04-03 00:03:53 -0700

    HackerRank assessment tests sample tests solutions.

---
+++ src/StairCase.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class StairCase {
+
+	static void StairCase(int n) {
+		char[] string = new char[n];
+		for (int i = 0; i < n; i++) {
+			string[i] = ' ';
+		}
+		int i = n - 1;
+		while (i >= 0) {
+			string[i--] = '#';
+			System.out.println(String.valueOf(string));
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int _n;
+		_n = Integer.parseInt(in.nextLine().trim());
+		StairCase(_n);
+	}
+
+}
---
+++ src/SumOfIntegers.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class SumOfIntegers {
+
+	static long sumOfIntegers(int[] arr) {
+		long sum = 0;
+		for (long i : arr) {
+			sum += i;
+		}
+		return sum;
+	}
+
+	public static void main(String[] args) throws IOException {
+		Scanner in = new Scanner(System.in);
+		// final String fileName = System.getenv("OUTPUT_PATH");
+		// BufferedWriter bw = new BufferedWriter(new FileWriter(fileName));
+		long res;
+
+		int _arr_size = 0;
+		_arr_size = Integer.parseInt(in.nextLine().trim());
+		int[] _arr = new int[_arr_size];
+		int _arr_item;
+		for (int _arr_i = 0; _arr_i < _arr_size; _arr_i++) {
+			_arr_item = Integer.parseInt(in.nextLine().trim());
+			_arr[_arr_i] = _arr_item;
+		}
+
+		res = sumOfIntegers(_arr);
+		// bw.write(String.valueOf(res));
+		// bw.newLine();
+		//
+		// bw.close();
+		System.out.println(res);
+	}
+
+}
