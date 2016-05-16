411eb7488a49f738bd187c3168367f8bb149b94a
milindhg <milindhg@gmail.com>
2016-05-16 16:34:36 -0400

    added argument for the followup question of setofstacks

--- src/ctci_5E/P3_3_SetOfStacks.java
+++ src/ctci_5E/P3_3_SetOfStacks.java
@@ -3,6 +3,36 @@ package ctci_5E;
 import java.util.ArrayList;
 import java.util.Stack;
 
+/**
+ * 
+ * @Problem: Imagine a (literal) stack of plates. If the stack gets too high, it
+ *           migh t topple. Therefore, in real life, we would likely start a new
+ *           stack when the previous stack exceeds some threshold. Implement a
+ *           data structure SetOfStacks that mimics this. SetOf- Stacks should
+ *           be composed of several stacks and should create a new stack once
+ *           the previous one exceeds capacity. SetOfStacks.push() and
+ *           SetOfStacks. pop () should behave identically to a single stack
+ *           (that is, pop () should return the same values as it would if there
+ *           were just a single stack).
+ * 
+ *           FOLLOW UP
+ * 
+ *           Implement a function popAt(int index) which performs a pop
+ *           operation on a specific sub-stack.
+ * 
+ * @Solution we can implement it using the arraylist of stacks and keeping a
+ *           size limit for each of the sub-stacks.
+ * 
+ *           For the follow up question, we can easily implement the push and
+ *           pop on a particular stack, however, we need to discuss with the
+ *           interviewer about the cases whether we need to adjust the smaller
+ *           stacks in the process, by shifting elements from the remaining
+ *           stacks on the empty/small stacks.
+ * 
+ * 
+ * @author Milind
+ *
+ */
 public class P3_3_SetOfStacks {
 	private ArrayList<Stack<Integer>> stacks;
 	private int maxStackSize;
@@ -43,6 +73,12 @@ public class P3_3_SetOfStacks {
 
 	public static void main(String[] args) {
 		P3_3_SetOfStacks stackSet = new P3_3_SetOfStacks(3);
+		try {
+			stackSet.pop();
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		stackSet.push(1);
 		System.out.println(stackSet);
 		stackSet.push(2);
