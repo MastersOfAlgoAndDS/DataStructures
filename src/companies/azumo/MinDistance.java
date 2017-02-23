afc0e29d1333b7260837b3d799d2939f54d0d1b0
Milind Gokhale <milindhg@gmail.com>
2017-02-22 23:59:17 -0800

    added comment to check the incomplete problem. Work in future to include a keyword in the problems to have all the TODOs in the list for easy grepping and completing the problems later. Also to keep the backup of incomplete problems in github for later working.

--- src/companies/azumo/MinDistance.java
+++ src/companies/azumo/MinDistance.java
@@ -3,6 +3,8 @@ package companies.azumo;
 import java.util.Arrays;
 
 /**
+ * 
+ * Check this problem whether it is minimum distance between consecutive numbers or any 2 numbers in the array.
  * Class to find the minimum distance between the values given in an array
  * 
  * @author Milind
@@ -20,7 +22,7 @@ class MinDistance {
 	}
 
 	public static void main(String[] args) {
-		int[] A = { 8, 24, 3, 20, 1, 17 };
+		int[] A = { 8, 24, 3, 20, 1, 19 };
 		MinDistance s = new MinDistance();
 		System.out.println(s.solution(A));
 	}
